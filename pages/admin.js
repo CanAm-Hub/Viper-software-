537b090f04b591b85344dbd830f42156de539465
CanAm-Hub <dougteskey75@gmail.com>
2025-10-22 18:29:29 -0400

    Create admin.js

---
+++ pages/admin.js
@@ -0,0 +1,12 @@
+export default function Admin() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Viper Admin</h1>
+      <ul>
+        <li>Send test SMS</li>
+        <li>View recent leads</li>
+        <li>Settings</li>
+      </ul>
+    </main>
+  );
+}
